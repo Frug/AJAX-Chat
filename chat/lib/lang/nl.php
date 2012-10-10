f138ecf192b0cd2cc59a77315a3de82902ddc2a9
Frug <frugen@gmail.com>
2012-10-10 15:46:25 -0400

    Updated everything to 0.8.5a
    Update readme.md

---
+++ chat/lib/lang/nl.php
@@ -0,0 +1,124 @@
+<?php
+/*
+ * @package AJAX_Chat
+ * @author Sebastian Tschan
+ * @author Nic Mertens
+ * @copyright (c) Sebastian Tschan
+ * @license GNU Affero General Public License
+ * @link https://blueimp.net/ajax/
+ */
+
+$lang = array();
+$lang['title'] = 'AJAX Chat';
+$lang['userName'] = 'Gebruikersnaam';
+$lang['password'] = 'Wachtwoord';
+$lang['login'] = 'Inloggen';
+$lang['logout'] = 'Uitloggen';
+$lang['channel'] = 'Kanaal';
+$lang['style'] = 'Style';
+$lang['language'] = 'Taal';
+$lang['inputLineBreak'] = 'Press SHIFT+ENTER to input a line break';
+$lang['messageSubmit'] = 'Verzend';
+$lang['registeredUsers'] = 'Geregistreerde gebruikers';
+$lang['onlineUsers'] = 'Online gebruikers';
+$lang['toggleAutoScroll'] = 'Autoscroll on/off';
+$lang['toggleAudio'] = 'Sound on/off';
+$lang['toggleHelp'] = 'Show/hide help';
+$lang['toggleSettings'] = 'Show/hide settings';
+$lang['toggleOnlineList'] = 'Show/hide online list';
+$lang['bbCodeLabelBold'] = 'b';
+$lang['bbCodeLabelItalic'] = 'i';
+$lang['bbCodeLabelUnderline'] = 'u';
+$lang['bbCodeLabelQuote'] = 'Quote';
+$lang['bbCodeLabelCode'] = 'Code';
+$lang['bbCodeLabelURL'] = 'URL';
+$lang['bbCodeLabelImg'] = 'Image';
+$lang['bbCodeLabelColor'] = 'Font Color';
+$lang['bbCodeTitleBold'] = 'Bold text: [b]text[/b]';
+$lang['bbCodeTitleItalic'] = 'Italic text: [i]text[/i]';
+$lang['bbCodeTitleUnderline'] = 'Underline text: [u]text[/u]';
+$lang['bbCodeTitleQuote'] = 'Quote text: [quote]text[/quote] or [quote=author]text[/quote]';
+$lang['bbCodeTitleCode'] = 'Code display: [code]code[/code]';
+$lang['bbCodeTitleURL'] = 'Voeg URL in: [url]http://example.org[/url] or [url=http://example.org]text[/url]';
+$lang['bbCodeTitleImg'] = 'Insert image: [img]http://example.org/image.jpg[/img]';
+$lang['bbCodeTitleColor'] = 'Font Color: [color=red]text[/color]';
+$lang['help'] = 'Help';
+$lang['helpItemDescJoin'] = 'Betreed een kanaal:';
+$lang['helpItemCodeJoin'] = '/join Kanaalnaam';
+$lang['helpItemDescJoinCreate'] = 'Maak een privékanaal (Enkel geregistreerde gebruikers):';
+$lang['helpItemCodeJoinCreate'] = '/join';
+$lang['helpItemDescInvite'] = 'Nodig iemand uit (bv. naar een privékanaal):';
+$lang['helpItemCodeInvite'] = '/invite Gebruikersnaam';
+$lang['helpItemDescUninvite'] = 'Annuleer de uitnodiging:';
+$lang['helpItemCodeUninvite'] = '/uninvite Gebruikersnaam';
+$lang['helpItemDescLogout'] = 'Uitloggen:';
+$lang['helpItemCodeLogout'] = '/quit';
+$lang['helpItemDescPrivateMessage'] = 'Privé bericht:';
+$lang['helpItemCodePrivateMessage'] = '/msg Gebruikersnaam Tekst';
+$lang['helpItemDescQueryOpen'] = 'Open een privékanaal:';
+$lang['helpItemCodeQueryOpen'] = '/query Gebruikersnaam';
+$lang['helpItemDescQueryClose'] = 'Sluit een privékanaal:';
+$lang['helpItemCodeQueryClose'] = '/query';
+$lang['helpItemDescAction'] = 'Beschrijf actie:';
+$lang['helpItemCodeAction'] = '/action Tekst';
+$lang['helpItemDescDescribe'] = 'Beschrijf actie in privébericht:';
+$lang['helpItemCodeDescribe'] = '/describe Gebruikersnaam Tekst';
+$lang['helpItemDescIgnore'] = 'Negeer/aanvaard berichten van gebruiker:';
+$lang['helpItemCodeIgnore'] = '/ignore Gebruikersnaam';
+$lang['helpItemDescIgnoreList'] = 'Toon de genegeerde gebruikers:';
+$lang['helpItemCodeIgnoreList'] = '/ignore';
+$lang['helpItemDescWhereis'] = 'Display user channel:';
+$lang['helpItemCodeWhereis'] = '/whereis Username';
+$lang['helpItemDescKick'] = 'Kick een gebruiker (Enkel Moderators of Administrators):';
+$lang['helpItemCodeKick'] = '/kick Gebruikersnaam [aantal minuten]';
+$lang['helpItemDescUnban'] = 'Unban een gebruiker (Enkel Moderators of Administrators):';
+$lang['helpItemCodeUnban'] = '/unban Gebruikersnaam';
+$lang['helpItemDescBans'] = 'Lijst van gebande Gebruikers (Enkel Moderators of Administrators):';
+$lang['helpItemCodeBans'] = '/bans';
+$lang['helpItemDescWhois'] = 'Toon gebruikers-IP (Enkel Moderators of Administrators):';
+$lang['helpItemCodeWhois'] = '/whois Gebruikersnaam';
+$lang['helpItemDescWho'] = 'Lijst van gebruikers online:';
+$lang['helpItemCodeWho'] = '/who [Kanaalnaam]';
+$lang['helpItemDescList'] = 'Lijst van beschikbare kanalen:';
+$lang['helpItemCodeList'] = '/list';
+$lang['helpItemDescRoll'] = 'Werp dobbelsteen:';
+$lang['helpItemCodeRoll'] = '/roll [number]d[sides]';
+$lang['helpItemDescNick'] = 'Change username:';
+$lang['helpItemCodeNick'] = '/nick Username';
+$lang['settings'] = 'Settings';
+$lang['settingsBBCode'] = 'Enable BBCode:';
+$lang['settingsBBCodeImages'] = 'Enable image BBCode:';
+$lang['settingsBBCodeColors'] = 'Enable font color BBCode:';
+$lang['settingsHyperLinks'] = 'Enable hyperlinks:';
+$lang['settingsLineBreaks'] = 'Enable line breaks:';
+$lang['settingsEmoticons'] = 'Enable emoticons:';
+$lang['settingsAutoFocus'] = 'Automatically set the focus on the input field:';
+$lang['settingsMaxMessages'] = 'Maximum number of messages in the chatlist:';
+$lang['settingsWordWrap'] = 'Enable wrapping of long words:';
+$lang['settingsMaxWordLength'] = 'Maximum length of a word before it gets wrapped:';
+$lang['settingsDateFormat'] = 'Format of date and time display:';
+$lang['settingsPersistFontColor'] = 'Persist font color:';
+$lang['settingsAudioVolume'] = 'Sound Volume:';
+$lang['settingsSoundReceive'] = 'Sound for incoming messages:';
+$lang['settingsSoundSend'] = 'Sound for outgoing messages:';
+$lang['settingsSoundEnter'] = 'Sound for login and channel enter messages:';
+$lang['settingsSoundLeave'] = 'Sound for logout and channel leave messages:';
+$lang['settingsSoundChatBot'] = 'Sound for chatbot messages:';
+$lang['settingsSoundError'] = 'Sound for error messages:';
+$lang['settingsBlink'] = 'Blink window title on new messages:';
+$lang['settingsBlinkInterval'] = 'Blink interval in milliseconds:';
+$lang['settingsBlinkIntervalNumber'] = 'Number of blink intervals:';
+$lang['playSelectedSound'] = 'Play selected sound';
+$lang['requiresJavaScript'] = 'JavaScript is nodig voor deze chat.';
+$lang['errorInvalidUser'] = 'Ongeldige gebruikersnaam.';
+$lang['errorUserInUse'] = 'Gebruikersnaam is in gebruik, kies een andere.';
+$lang['errorBanned'] = 'Gebruiker of gebruikers-IP is gebanned.';
+$lang['errorMaxUsersLoggedIn'] = 'Dit chatkanaal heeft het maximum toegelaten aantal ingelogde gebruikers bereikt.';
+$lang['errorChatClosed'] = 'Dit chatkanaal is momenteel gesloten.';
+$lang['logsTitle'] = 'AJAX Chat - Logs';
+$lang['logsDate'] = 'Datum';
+$lang['logsTime'] = 'Tijd';
+$lang['logsSearch'] = 'Zoek';
+$lang['logsPrivateChannels'] = 'Privékanalen';
+$lang['logsPrivateMessages'] = 'Privéberichten';
+?>
---
+++ readme.md
@@ -0,0 +1,30 @@
+![Alt text](/Frug/AJAX-Chat/wiki/MediaWikiSidebarLogo.png)
+AJAX Chat Standalone
+=========
+
+This is the standalone version of Blueimp's AJAX Chat. If you are looking for a version to integrate with your forum, select a different branch.
+
+AJAX stands for "Asynchronous JavaScript and XML".
+The AJAX Chat clients (the user browsers) use JavaScript to query the web server for updates.
+Instead of delivering a complete HTML page only updated data is send in XML format.
+By using JavaScript the chat page can be updated without having to reload the whole page.
+
+
+Requirements
+============
+
+###Server-Side:
+- PHP >= 4
+- MySQL >= 4
+- Ruby >= 1.8 (optional)
+
+
+###Client-Side:
+- Enabled JavaScript
+- Enabled Cookies
+- Flash Plugin >= 9 (optional)
+
+Help
+====
+
+For further help on instaling and using AJAX Chat, consult the wiki on github at https://github.com/Frug/AJAX-Chat/wiki or the readme files included in the project download.
