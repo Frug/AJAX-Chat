8c16ca0c83dbf524cb260794b90f360a025a2aae
Frug <frugen@gmail.com>
2012-10-20 11:29:06 -0400

    phpbb3 diff files

--- chat/lib/custom.php
+++ chat/lib/custom.php
@@ -5,7 +5,18 @@
  * @copyright (c) Sebastian Tschan
  * @license Modified MIT License
  * @link https://blueimp.net/ajax/
+ * 
+ * phpBB3 integration:
+ * http://www.phpbb.com/
  */
 
-// Include custom libraries and initialization code here
+// phpBB initialization:		
+define('IN_PHPBB', true);
+$phpbb_root_path = AJAX_CHAT_PATH.'../';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+require($phpbb_root_path.'common.'.$phpEx);
+
+// phpBB session management:
+$user->session_begin();
+$auth->acl($user->data);
 ?>
