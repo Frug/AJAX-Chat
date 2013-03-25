439c5796ae0472bfb021888abd277eee7457c1fa
Clint Harrison <clinteger@gmail.com>
2013-03-24 20:59:42 -0400

    Actually show all errors

--- chat/index.php
+++ chat/index.php
@@ -8,7 +8,7 @@
  */
 
 // Show all errors:
-error_reporting(E_ALL);
+error_reporting(E_ALL | E_STRICT);
 
 // Path to the chat directory:
 define('AJAX_CHAT_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
