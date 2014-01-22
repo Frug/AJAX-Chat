b11020522470fe4e6d039e47953766c1a3645c1d
Frug <frugen@gmail.com>
2014-01-22 13:21:14 -0500

    add punbb integration

--- chat/lib/integration/punbb/CustomAJAXChat.php
+++ chat/lib/integration/punbb/CustomAJAXChat.php
@@ -3,16 +3,15 @@
  * @package AJAX_Chat
  * @author Sebastian Tschan
  * @copyright (c) Sebastian Tschan
- * @license Modified MIT License
+ * @license GNU Affero General Public License
  * @link https://blueimp.net/ajax/
  * 
- * MyBB integration:
- * http://www.mybboard.net/
+ * PunBB integration:
+ * http://punbb.org/
  */
  
-define('IN_MYBB', 1);
-chdir(AJAX_CHAT_PATH.'../');
-require(AJAX_CHAT_PATH.'../global.php');
+define('PUN_ROOT', AJAX_CHAT_PATH.'../');
+require PUN_ROOT.'include/common.php';
 
 class CustomAJAXChat extends AJAXChat {
 
@@ -20,35 +19,35 @@ class CustomAJAXChat extends AJAXChat {
 	function initCustomConfig() {
 		global $db;
 		
-		// Use the existing MyBB database connection:
-		$this->setConfig('dbConnection', 'link', $db->current_link);
+		// Use the existing PunBB database connection:
+		$this->setConfig('dbConnection', 'link', $db->link_id);
 	}
 
 	// Initialize custom request variables:
 	function initCustomRequestVars() {
-		global $mybb;
+		global $pun_user;
 
-		// Auto-login MyBB users:
-		if(!$this->getRequestVar('logout') && $mybb->user['uid']) {
+		// Auto-login phpBB users:
+		if(!$this->getRequestVar('logout') && !$pun_user['is_guest']) {
 			$this->setRequestVar('login', true);
 		}
 	}
 
 	// Replace custom template tags:
-	function replaceCustomTemplateTags($tag, $tagContent) {	
-		global $mybb;
-		
+	function replaceCustomTemplateTags($tag, $tagContent) {
+		global $pun_user,$pun_config;
+
 		switch($tag) {
 
 			case 'FORUM_LOGIN_URL':
-				if($mybb->user['uid']) {
+				if(!$pun_user['is_guest']) {
 					return ($this->getRequestVar('view') == 'logs') ? './?view=logs' : './';
 				} else {
-					return $mybb->settings['bburl'].'/member.php';
+					return $this->htmlEncode($pun_config['o_base_url'].'/login.php?action=in');
 				}
 				
 			case 'REDIRECT_URL':
-				if($mybb->user['uid']) {
+				if(!$pun_user['is_guest']) {
 					return '';
 				} else {
 					return $this->htmlEncode($this->getRequestVar('view') == 'logs' ? $this->getChatURL().'?view=logs' : $this->getChatURL());
@@ -62,9 +61,9 @@ class CustomAJAXChat extends AJAXChat {
 	// Returns true if the userID of the logged in user is identical to the userID of the authentication system
 	// or the user is authenticated as guest in the chat and the authentication system
 	function revalidateUserID() {
-		global $mybb;
+		global $pun_user;
 		
-		if($this->getUserRole() === AJAX_CHAT_GUEST && !$mybb->user['uid'] || ($this->getUserID() === $mybb->user['uid'])) {
+		if($this->getUserRole() === AJAX_CHAT_GUEST && $pun_user['is_guest'] || ($this->getUserID() === $pun_user['id'])) {
 			return true;
 		}
 		return false;
@@ -73,19 +72,18 @@ class CustomAJAXChat extends AJAXChat {
 	// Returns an associative array containing userName, userID and userRole
 	// Returns null if login is invalid
 	function getValidLoginUserData() {
-		global $mybb;
+		global $pun_user;
 		
 		// Check if we have a valid registered user:
-		if($mybb->user['uid']) {
+		if(!$pun_user['is_guest']) {
 			$userData = array();
-			$userData['userID'] = $mybb->user['uid'];
-
-			$userData['userName'] = $this->trimUserName($mybb->user['username']);
+			$userData['userID'] = $pun_user['id'];
 			
-			// Take the userrole from the MyBB users primary group:
-			if($mybb->user['usergroup'] == 4)
+			$userData['userName'] = $this->trimUserName($pun_user['username']);
+			
+			if($pun_user['g_id'] == PUN_ADMIN)
 				$userData['userRole'] = AJAX_CHAT_ADMIN;
-			else if($mybb->user['usergroup'] == 3)
+			elseif($pun_user['g_id'] == PUN_MOD)
 				$userData['userRole'] = AJAX_CHAT_MODERATOR;
 			else
 				$userData['userRole'] = AJAX_CHAT_USER;
@@ -102,27 +100,50 @@ class CustomAJAXChat extends AJAXChat {
 	// Make sure channel names don't contain any whitespace
 	function &getChannels() {
 		if($this->_channels === null) {
+			global $db,$pun_user;
+			
 			$this->_channels = array();
+			
+			// Get valid PunBB forums:
+			$sql = 'SELECT
+						id,
+						forum_name
+					FROM
+						'.$db->prefix.'forums AS f
+					LEFT JOIN
+						'.$db->prefix.'forum_perms AS fp
+					ON
+						(fp.forum_id=f.id AND fp.group_id=\''.$db->escape($pun_user['g_id']).'\')
+					WHERE
+						(fp.read_forum IS NULL OR fp.read_forum=1);';
+			$result = $db->query($sql);
 
-			$allChannels = $this->getAllChannels();
+			$defaultChannelFound = false;
 
-			// Build the forum permissions for all forums:
-			$forumPermissions = forum_permissions();
-
-			foreach($allChannels as $key=>$value) {
-				if ($value == $this->getConfig('defaultChannelID')) {
-					$this->_channels[$key] = $value;
-					continue;
-				}
+			while ($row = $db->fetch_assoc($result)) {
 				// Check if we have to limit the available channels:
-				if($this->getConfig('limitChannelList') && !in_array($value, $this->getConfig('limitChannelList'))) {
+				if($this->getConfig('limitChannelList') && !in_array($row['id'], $this->getConfig('limitChannelList'))) {
 					continue;
 				}
 
-				// Add the valid channels to the channel list (the defaultChannelID is always valid):
-				if($forumPermissions[$value]['canview'] == 'yes' ||  $value == $this->getConfig('defaultChannelID')) {
-					$this->_channels[$key] = $value;
+				$forumName = $this->trimChannelName($row['forum_name']);
+				
+				$this->_channels[$forumName] = $row['id'];
+
+				if(!$defaultChannelFound && $row['id'] == $this->getConfig('defaultChannelID')) {
+					$defaultChannelFound = true;
 				}
+			}
+			$db->free_result($result);
+
+			if(!$defaultChannelFound) {
+				// Add the default channel as first array element to the channel list:
+				$this->_channels = array_merge(
+					array(
+						$this->trimChannelName($this->getConfig('defaultChannelName'))=>$this->getConfig('defaultChannelID')
+					),
+					$this->_channels
+				);
 			}
 		}
 		return $this->_channels;
@@ -132,36 +153,33 @@ class CustomAJAXChat extends AJAXChat {
 	// Make sure channel names don't contain any whitespace
 	function &getAllChannels() {
 		if($this->_allChannels === null) {
-			global $db;
-
+			global $db,$pun_user;
+			
 			$this->_allChannels = array();
-
-			// Get all MyBB forums:
+			
+			// Get all PunBB forums:
 			$sql = 'SELECT
-							fid,
-							name
-						FROM
-							'.TABLE_PREFIX.'forums
-						WHERE
-							type=\'f\';';
+						id,
+						forum_name
+					FROM
+						'.$db->prefix.'forums;';
 			$result = $db->query($sql);
 
 			$defaultChannelFound = false;
 
-			while ($row = $db->fetch_array($result)) {
-				$forumName = $this->trimChannelName($row['name']);
+			while ($row = $db->fetch_assoc($result)) {
+				$forumName = $this->trimChannelName($row['forum_name']);
+				
+				$this->_allChannels[$forumName] = $row['id'];
 
-				$this->_allChannels[$forumName] = $row['fid'];
-
-				if(!$defaultChannelFound && $row['fid'] == $this->getConfig('defaultChannelID')) {
+				if(!$defaultChannelFound && $row['id'] == $this->getConfig('defaultChannelID')) {
 					$defaultChannelFound = true;
 				}
 			}
+			$db->free_result($result);
 
 			if(!$defaultChannelFound) {
-				// Add the default channel as first array element to the channel list
-				// First remove it in case it appeard under a different ID
-				unset($this->_allChannels[$this->getConfig('defaultChannelName')]);
+				// Add the default channel as first array element to the channel list:
 				$this->_allChannels = array_merge(
 					array(
 						$this->trimChannelName($this->getConfig('defaultChannelName'))=>$this->getConfig('defaultChannelID')
@@ -173,14 +191,14 @@ class CustomAJAXChat extends AJAXChat {
 		return $this->_allChannels;
 	}
 
-	// Method to set the style cookie depending on the MyBB user style
+	// Method to set the style cookie depending on the PunBB user style:
 	function setStyle() {
-		global $theme;
+		global $pun_user;
 		
 		if(isset($_COOKIE[$this->getConfig('sessionName').'_style']) && in_array($_COOKIE[$this->getConfig('sessionName').'_style'], $this->getConfig('styleAvailable')))
 			return;
-
-		$styleName = $theme['name'];
+		
+		$styleName = $pun_user['style'];
 		
 		if(!in_array($styleName, $this->getConfig('styleAvailable'))) {
 			$styleName = $this->getConfig('styleDefault');
