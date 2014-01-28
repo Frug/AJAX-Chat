78ede4be6ee8ad2dbbb401e0d6eadf44cd6e6640
marquisite <marquisite@users.noreply.github.com>
2014-01-28 23:54:11 +1030

    Custom user group now uses different text colour
    Plus one edit to chat.js to give the new AJAX_CHAT_CUSTOM user group the
    ability to delete their own messages, just like AJAX_CHAT_USERS is able
    to.

--- chat/js/chat.js
+++ chat/js/chat.js
@@ -1334,6 +1334,9 @@ var ajaxChat = {
 		if((((this.userRole === '1' && this.allowUserMessageDelete && (userID === this.userID ||
 			parseInt(channelID) === parseInt(this.userID)+this.privateMessageDiff ||
 			parseInt(channelID) === parseInt(this.userID)+this.privateChannelDiff)) ||
+			(this.userRole === '5' && this.allowUserMessageDelete && (userID == this.userID ||
+			parseInt(channelID) === parseInt(this.userID)+this.privateMessageDiff ||
+			parseInt(channelID) === parseInt(this.userID)+this.privateChannelDiff)) ||
 			this.userRole === '2') && userRole !== '3' && userRole !== '4') || this.userRole === '3') {
 			return true;
 		}
