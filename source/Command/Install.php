d596d507d1f283c08721b8c1f2c5358d6993b102
stevleibelt <stev@leibelt.de>
2014-08-19 16:00:43 +0200

    added check for not existing directories

--- source/Command/Install.php
+++ source/Command/Install.php
@@ -62,6 +62,12 @@ class Command_Install extends Command_AbstractCommand
 
         foreach ($identifierToPaths as $identifier => $paths) {
             if (!$this->filesystem->isFile($paths['chat'])) {
+                $directoryPath = dirname($paths['chat']);
+
+                if (!$this->filesystem->isDirectory($directoryPath)) {
+                    $this->output->addLine('no directory available at "' . $directoryPath . '", will create one ...');
+                    $this->filesystem->createDirectory($directoryPath);
+                }
                 $this->output->addLine('no ' . $identifier . ' file available, will create one ...');
                 $this->filesystem->copy(
                     $paths['example'],
