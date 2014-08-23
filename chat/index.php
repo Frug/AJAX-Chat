5d8755faf06fd2ff9d3977f103e641b0ba9f714e
stevleibelt <stev@leibelt.de>
2014-08-23 16:03:07 +0200

    reverted unwished change (whooops) from index.php

--- chat/index.php
+++ chat/index.php
@@ -1,2 +1,24 @@
 <?php
-echo 'updating system, back soon';
+/*
+ * @package AJAX_Chat
+ * @author Sebastian Tschan
+ * @author Philip Nicolcev
+ * @copyright (c) Sebastian Tschan
+ * @license Modified MIT License
+ * @link https://blueimp.net/ajax/
+ */
+
+// Suppress errors:
+error_reporting(E_ALL);
+
+// Path to the chat directory:
+define('AJAX_CHAT_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
+
+// Include custom libraries and initialization code:
+require(AJAX_CHAT_PATH.'lib/custom.php');
+
+// Include Class libraries:
+require(AJAX_CHAT_PATH.'lib/classes.php');
+
+// Initialize the chat:
+$ajaxChat = new CustomAJAXChat();
